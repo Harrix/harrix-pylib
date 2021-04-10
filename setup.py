89d84c4aedafe44e7e483ac9ef91fc8c0f73f445
Sergienko Anton <sergienkoanton@mail.ru>
2021-04-10 20:31:16 +0300

    Build library v0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='harrix-pylib',
-      version='0.2',
+      version='0.3',
       description='Different functions',
       packages=['harrixpylib'],
       author_email='anton.b.sergienko@gmail.com',
