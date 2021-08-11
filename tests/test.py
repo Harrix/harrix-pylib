f568ece2208e7c4c14c7a5546e14fe1a629dad84
Sergienko Anton <sergienkoanton@mail.ru>
2021-08-11 16:35:08 +0300

    Add tests

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,25 @@
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+import unittest
+import os
+from pathlib import Path
 
 import harrixpylib as h
 
-print(h.open_file("C:/Windows/System32/drivers/etc/hosts"))
+currentdir = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+class TestHarrixpylib(unittest.TestCase):
+
+    def test_open_file(self):
+        s = h.open_file(currentdir / 'data/simple_text.txt')
+        self.assertEqual(s, '42')
+
+    def test_path_to_pathlib(self):
+        self.assertEqual(h.path_to_pathlib(
+            os.path.dirname(os.path.realpath(__file__))), currentdir)
+
+    def test_clear_directory(self):
+        h.clear_directory(currentdir / 'test_dir')
+
+
+if __name__ == '__main__':
+    unittest.main()
