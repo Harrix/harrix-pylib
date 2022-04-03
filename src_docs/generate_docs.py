c50f9de9fbe2352d0468937260c8be17da3b2d93
Sergienko Anton <sergienkoanton@mail.ru>
2022-04-04 01:54:33 +0300

    Add a folder existence check

--- src_docs/generate_docs.py
+++ src_docs/generate_docs.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
     root_folder = Path(__file__).resolve().parent.parent
     h.clear_directory(root_folder / "docs")
     os.system('pdoc -o ./docs --docformat="google" src\\harrixpylib\\')
-    shutil.copytree(
-        root_folder / "src_docs/img",
-        root_folder / "docs/harrixpylib/img",
-        dirs_exist_ok=True,
-    )
+    path_img = root_folder / "src_docs/img"
+    if path_img.is_dir():
+        shutil.copytree(
+            path_img,
+            root_folder / "docs/harrixpylib/img",
+            dirs_exist_ok=True,
+        )
     url = f"file:///{str(root_folder)}/docs/index.html"
     webbrowser.open(url, new=2)
