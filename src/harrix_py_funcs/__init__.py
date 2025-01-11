d94608648ea7d9d8c44624604e025fd52dec542e
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-11 11:38:57 +0300

    Refactor. Tranform to package

---
+++ src/harrix_py_funcs/__init__.py
@@ -0,0 +1,4 @@
+from harrix_py_funcs import funcs_dev as dev  # noqa
+from harrix_py_funcs import funcs_file as file  # noqa
+from harrix_py_funcs import funcs_md as md  # noqa
+from harrix_py_funcs import funcs_pyside as pyside  # noqa
