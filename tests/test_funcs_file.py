e5a8e0d8ed7cccc39c558c8c636f698217f9e6ff
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-12 17:42:38 +0300

    Rename tests

--- tests/test_funcs_file.py
+++ tests/test_funcs_file.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import harrix_pylib as h
 
 
-def test_clear_directory__01():
+def test_clear_directory():
     folder = h.dev.get_project_root() / "tests/data/temp"
     folder.mkdir(parents=True, exist_ok=True)
     Path(folder / "temp.txt").write_text("Hello, world!", encoding="utf8")
--- tests/test_funcs_md.py
+++ tests/test_funcs_md.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import harrix_pylib as h
 
 
-def test_get_yaml_from_markdown__01():
+def test_get_yaml_from_markdown():
     current_folder = h.dev.get_project_root()
     filename = current_folder / "tests/data/article.md"
     md = Path(filename).read_text(encoding="utf8")
@@ -12,7 +12,7 @@ def test_get_yaml_from_markdown__01():
     assert len(md_clean.splitlines()) == 4
 
 
-def test_remove_yaml_from_markdown__01():
+def test_remove_yaml_from_markdown():
     current_folder = h.dev.get_project_root()
     filename = current_folder / "tests/data/article.md"
     md = Path(filename).read_text(encoding="utf8")
