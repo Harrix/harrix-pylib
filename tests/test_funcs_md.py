4d0013c6911818b22f3b4ee3a18cdbb039e26e84
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-16 22:43:59 +0300

    Rename MD file

--- tests/test_funcs_md.py
+++ tests/test_funcs_md.py
@@ -1,19 +1,16 @@
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import harrix_pylib as h
 
 
 def test_get_yaml_from_markdown():
-    current_folder = h.dev.get_project_root()
-    filename = current_folder / "tests/data/article.md"
-    md = Path(filename).read_text(encoding="utf8")
+    md = Path(h.dev.get_project_root() / "tests/data/get_yaml.md").read_text(encoding="utf8")
     md_clean = h.md.get_yaml(md)
     assert len(md_clean.splitlines()) == 4
 
 
 def test_remove_yaml_from_markdown():
-    current_folder = h.dev.get_project_root()
-    filename = current_folder / "tests/data/article.md"
-    md = Path(filename).read_text(encoding="utf8")
+    md = Path(h.dev.get_project_root() / "tests/data/get_yaml.md").read_text(encoding="utf8")
     md_clean = h.md.remove_yaml(md)
     assert len(md_clean.splitlines()) == 1
