851263d3505aec9aed04b77a8cb292317c07abf2
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-16 22:46:57 +0300

    Add test def test_sort_sections()

--- tests/test_funcs_md.py
+++ tests/test_funcs_md.py
@@ -14,3 +14,18 @@ def test_remove_yaml_from_markdown():
     md = Path(h.dev.get_project_root() / "tests/data/get_yaml.md").read_text(encoding="utf8")
     md_clean = h.md.remove_yaml(md)
     assert len(md_clean.splitlines()) == 1
+
+
+def test_sort_sections():
+    current_folder = h.dev.get_project_root()
+    md = Path(current_folder / "tests/data/sort_sections__01.md").read_text(encoding="utf8")
+    md_sorted = Path(current_folder / "tests/data/sort_sections__02.md").read_text(encoding="utf8")
+
+    with TemporaryDirectory() as temp_dir:
+        temp_filename = Path(temp_dir) / "temp.md"
+        temp_filename.write_text(md, encoding="utf-8")
+        h.md.sort_sections(temp_filename)
+        md_temp_sorted = temp_filename.read_text(encoding="utf8")
+        print(md_temp_sorted)
+
+    assert md_sorted == md_temp_sorted
