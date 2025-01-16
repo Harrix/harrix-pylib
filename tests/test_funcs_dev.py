e532589559c64fa7699851b39c0227bcba0cefca
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-17 02:19:11 +0300

    Add tests

--- tests/test_funcs_dev.py
+++ tests/test_funcs_dev.py
@@ -1,3 +1,7 @@
+import subprocess
+
+import pytest
+
 import harrix_pylib as h
 
 
@@ -10,3 +14,29 @@ def test_get_project_root():
 def test_get_project_root():
     config = h.dev.load_config(h.dev.get_project_root() / "tests/data/config.json")
     assert config["path_github"] == "C:/GitHub"
+
+
+@pytest.mark.skipif(
+    not subprocess.run(["powershell", "-Command", "echo test"], capture_output=True, text=True).returncode == 0,
+    reason="PowerShell is not available",
+)
+def test_run_powershell_script():
+    test_commands = "Write-Output 'Hello, World!'"
+    expected_output = "Hello, World!\n"
+
+    output = h.dev.run_powershell_script(test_commands)
+
+    assert output.strip() == expected_output.strip()
+
+
+@pytest.mark.skipif(
+    not subprocess.run(["powershell", "-Command", "echo test"], capture_output=True, text=True).returncode == 0,
+    reason="PowerShell is not available",
+)
+def test_run_powershell_script_as_admin():
+    test_commands = "Write-Output 'Hello, World!'"
+    expected_output = "Hello, World!\n"
+
+    output = h.dev.run_powershell_script_as_admin(test_commands)
+
+    assert output.strip() == "\ufeff" + expected_output.strip()
