84986889c53e6129fd8fe061f3739a39c3dbd5ef
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-16 23:48:15 +0300

    Add tests

--- tests/test_funcs_md.py
+++ tests/test_funcs_md.py
@@ -4,6 +4,21 @@ from tempfile import TemporaryDirectory
 import harrix_pylib as h
 
 
+def test_add_image_captions():
+    current_folder = h.dev.get_project_root()
+    md = Path(current_folder / "tests/data/add_image_captions__before.md").read_text(encoding="utf8")
+    md_after = Path(current_folder / "tests/data/add_image_captions__after.md").read_text(encoding="utf8")
+
+    with TemporaryDirectory() as temp_folder:
+        temp_filename = Path(temp_folder) / "temp.md"
+        temp_filename.write_text(md, encoding="utf-8")
+        h.md.add_image_captions(temp_filename)
+        md_applied = temp_filename.read_text(encoding="utf8")
+        print(md_applied)
+
+    assert md_after == md_applied
+
+
 def test_get_yaml_from_markdown():
     md = Path(h.dev.get_project_root() / "tests/data/get_yaml.md").read_text(encoding="utf8")
     md_clean = h.md.get_yaml(md)
@@ -18,14 +33,14 @@ def test_remove_yaml_from_markdown():
 
 def test_sort_sections():
     current_folder = h.dev.get_project_root()
-    md = Path(current_folder / "tests/data/sort_sections__01.md").read_text(encoding="utf8")
-    md_sorted = Path(current_folder / "tests/data/sort_sections__02.md").read_text(encoding="utf8")
+    md = Path(current_folder / "tests/data/sort_sections__before.md").read_text(encoding="utf8")
+    md_after = Path(current_folder / "tests/data/sort_sections__after.md").read_text(encoding="utf8")
 
-    with TemporaryDirectory() as temp_dir:
-        temp_filename = Path(temp_dir) / "temp.md"
+    with TemporaryDirectory() as temp_folder:
+        temp_filename = Path(temp_folder) / "temp.md"
         temp_filename.write_text(md, encoding="utf-8")
         h.md.sort_sections(temp_filename)
-        md_temp_sorted = temp_filename.read_text(encoding="utf8")
-        print(md_temp_sorted)
+        md_applied = temp_filename.read_text(encoding="utf8")
+        print(md_applied)
 
-    assert md_sorted == md_temp_sorted
+    assert md_after == md_applied
