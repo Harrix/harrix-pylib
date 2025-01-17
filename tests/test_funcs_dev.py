129fefbb2762485793346cf6f6867068658dc22a
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-17 09:49:08 +0300

    Add test def test_sort_py_code()

--- tests/test_funcs_dev.py
+++ tests/test_funcs_dev.py
@@ -1,4 +1,6 @@
 import subprocess
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -40,3 +42,17 @@ def test_run_powershell_script_as_admin():
     output = h.dev.run_powershell_script_as_admin(test_commands)
 
     assert output.strip() == "\ufeff" + expected_output.strip()
+
+
+def test_sort_py_code():
+    current_folder = h.dev.get_project_root()
+    py = Path(current_folder / "tests/data/sort_py_code__before.txt").read_text(encoding="utf8")
+    py_after = Path(current_folder / "tests/data/sort_py_code__after.txt").read_text(encoding="utf8")
+
+    with TemporaryDirectory() as temp_folder:
+        temp_filename = Path(temp_folder) / "temp.py"
+        temp_filename.write_text(py, encoding="utf-8")
+        h.dev.sort_py_code(temp_filename, True)
+        py_applied = temp_filename.read_text(encoding="utf8")
+
+    assert py_after == py_applied
