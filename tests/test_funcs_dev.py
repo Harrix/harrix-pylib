11848690f53de6b6769807fdc47fed6cfb9b34b5
Sergienko Anton <sergienkoanton@mail.ru>
2025-01-18 10:44:32 +0300

    Move two funcs to test_funcs_py.py from test_funcs_dev.py

--- tests/test_funcs_dev.py
+++ tests/test_funcs_dev.py
@@ -1,7 +1,5 @@
 import shutil
 import subprocess
-from pathlib import Path
-from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -12,20 +10,6 @@ def test_get_project_root():
     path = h.dev.get_project_root()
     assert "harrix-pylib" in str(path)
     assert (path / "tests").is_dir()
-
-
-def test_lint_and_fix_python_code():
-    python_code = "def greet(name):\n    print('Hello, ' +    name)"
-    expected_formatted_code = 'def greet(name):\n    print("Hello, " + name)\n'
-
-    formatted_code = h.dev.lint_and_fix_python_code(python_code)
-    assert formatted_code.strip() == expected_formatted_code.strip()
-
-    empty_code = ""
-    assert h.dev.lint_and_fix_python_code(empty_code) == empty_code
-
-    well_formatted_code = 'def greet(name):\n    print(f"Hello, {name}")\n'
-    assert h.dev.lint_and_fix_python_code(well_formatted_code) == well_formatted_code
 
 
 def test_load_config():
@@ -55,20 +39,6 @@ def test_run_powershell_script():
 #     expected_output = "Hello, World!\n"
 #     output = h.dev.run_powershell_script_as_admin(test_commands)
 #     assert output.strip() == "\ufeff" + expected_output.strip()
-
-
-def test_sort_py_code():
-    current_folder = h.dev.get_project_root()
-    py = Path(current_folder / "tests/data/sort_py_code__before.txt").read_text(encoding="utf8")
-    py_after = Path(current_folder / "tests/data/sort_py_code__after.txt").read_text(encoding="utf8")
-
-    with TemporaryDirectory() as temp_folder:
-        temp_filename = Path(temp_folder) / "temp.py"
-        temp_filename.write_text(py, encoding="utf-8")
-        h.dev.sort_py_code(temp_filename, True)
-        py_applied = temp_filename.read_text(encoding="utf8")
-
-    assert py_after == py_applied
 
 
 def test_write_in_output_txt():
